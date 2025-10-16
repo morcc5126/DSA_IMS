6d52eece91c20d81673fe25af45e0aa931de6f97
morcc5126 <168000650+morcc5126@users.noreply.github.com>
2025-10-16 19:15:06 +0800

    Add files via upload

---
+++ DSA_UI.java
@@ -0,0 +1,65 @@
+
+package dsa_ui;
+
+public class DSA_UI {
+
+    public static void main(String[] args) {
+        System.out.println("=========== Solar System Extraction ===========");
+        System.out.println("[1] Log-In");
+        System.out.println("[2] Exit");
+        System.out.println("Options (1-2): ");
+        
+        System.out.println("");
+        
+        System.out.println("=========== Solar System Extraction ===========");
+        System.out.println("[1] 📦Veiw Current Stocks");
+        System.out.println("[2] Update Stocks");
+        System.out.println("[3] Log-out");
+        System.out.println("[4] Exit");
+        
+        System.out.println("");
+        
+        //If [1] was picked
+        System.out.println("=========== Solar System Extraction ===========");
+        System.out.println("[1] 📦Complete Inventory");
+        System.out.println("[2] Current Suppliers");
+        System.out.println("[3] Available Minerals");
+        System.out.println("[4] Exit");
+        
+        System.out.println("");
+        
+        //If [2] was picked
+        System.out.println("=========== Solar System Extraction ===========");
+        System.out.println("[1] 📦Add");
+        System.out.println("[2] Edit");
+        System.out.println("[3] Remove");
+        System.out.println("[4] Exit");
+
+        //If (1) was picked
+        System.out.println("=========== Solar System Extraction ===========");
+        System.out.println("Mineral: ");
+        System.out.println("Supplier: ");
+        System.out.println("Quantity: ");
+        
+        // (2) was picked
+        System.out.println("=========== Solar System Extraction ===========");
+        System.out.println("[1] Edit Minerals");
+        System.out.println("[2] Edit Supplier");
+        System.out.println("[3] Edit Quantity");
+        System.out.println("[4] Edit Minerals");
+        System.out.println("");
+        
+        //If [3] was picked
+        System.out.println("=========== Solar System Extraction ===========");
+        System.out.println("Mineral: Bauxite");
+        System.out.println("Supplier: Mars");
+        System.out.println("Quantity: 10,000");
+        
+        System.out.println("");
+        
+        System.out.println("Mineral: Magnesium");
+        System.out.println("Supplier: Earth");
+        System.out.println("Quantity: 10,000");
+    }
+    
+}
